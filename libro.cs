a3f925ea4ef7baaeb5eb1ce171e953063e1bad7b
fati-p <143958473+fati-p@users.noreply.github.com>
2023-10-22 21:49:46 -0600

    Update libro.cs
    ejercicios3

--- libro.cs
+++ libro.cs
@@ -2,18 +2,31 @@ using System;
 
 public class Libro
 {
-    // Propiedades
     public string Título { get; set; }
     public string Autor { get; set; }
 
-  
-   
+    public Libro(string título, string autor)
+    {
+        Título = título;
+        Autor = autor;
+    }
+
+    public void MostrarInformacion()
+    {
+        Console.WriteLine($"Título: {Título}, Autor: {Autor}");
+    }
 }
 
 class Program
 {
     static void Main(string[] args)
     {
+        Libro libro1 = new Libro("Cien años de soledad", "Gabriel García Márquez");
+        Libro libro2 = new Libro("Don Quijote de la Mancha", "Miguel de Cervantes");
+
+        libro1.MostrarInformacion();
+        libro2.MostrarInformacion();
+
         Console.WriteLine("¡Hola, mundo!");
     }
 }
