97c7c6d42419824270873a043fc48e023d2e5b3a
fati-p <143958473+fati-p@users.noreply.github.com>
2023-10-22 21:55:47 -0600

    Update empleado.cs
    ejercicio 5

--- empleado.cs
+++ empleado.cs
@@ -1,15 +1,25 @@
+
 using System;
 
 public class Empleado
 {
-   
     public string Nombre { get; set; }
     public double Salario { get; set; }
 
-    
+    public Empleado(string nombre, double salario)
+    {
+        Nombre = nombre;
+        Salario = salario;
+    }
+
     public double CalcularSalarioAnual()
     {
-        
+        return Salario * 12;
+    }
+
+    public void MostrarInformacion()
+    {
+        Console.WriteLine($"Nombre: {Nombre}, Salario Mensual: {Salario}, Salario Anual: {CalcularSalarioAnual()}");
     }
 }
 
@@ -17,6 +27,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("¡Hola, mundo!");
+        Empleado empleado1 = new Empleado("Juan", 2000);
+        Empleado empleado2 = new Empleado("Ana", 2500);
+
+        empleado1.MostrarInformacion();
+        empleado2.MostrarInformacion();
+
+        Console.WriteLine("¡listo!");
     }
 }
