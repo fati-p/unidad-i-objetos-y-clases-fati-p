3a446e715ef64d74478e04ff3f3697234b32e06c
fati-p <143958473+fati-p@users.noreply.github.com>
2023-10-22 21:46:53 -0600

    Update pelicula.cs
    ejercicio2

--- pelicula.cs
+++ pelicula.cs
@@ -2,16 +2,34 @@ using System;
 
 public class Película
 {
-    
-    public string Titulo { get; set; }
-    public int AnyoLanzamiento { get; set; }
-   
+    public string Título { get; set; }
+    public int AñoLanzamiento { get; set; }
+    public int Calificación { get; set; }
+
+    public Película(string título, int añoLanzamiento, int calificación)
+    {
+        Título = título;
+        AñoLanzamiento = añoLanzamiento;
+        Calificación = calificación;
+    }
+
+    public void MostrarInformación()
+    {
+        Console.WriteLine($"Título: {Titulo}, Año de Lanzamiento: {AñoLanzamiento}, Calificación: {Calificación}");
+    }
 }
 
 class Program
 {
     static void Main(string[] args)
     {
+        Película película1 = new Película("El Padrino", 1972, 5);
+        Película película2 = new Película("Pulp Fiction", 1994, 4);
+
+        película1.MostrarInformación();
+        película2.MostrarInformación();
+
         Console.WriteLine("¡Hola, mundo!");
     }
 }
+
