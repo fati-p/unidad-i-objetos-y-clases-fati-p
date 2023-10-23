de918d811b5181368d1d3565f143d8772380b579
fati-p <143958473+fati-p@users.noreply.github.com>
2023-10-22 21:52:45 -0600

    Update estudiante.cs
    ejercicio4

--- estudiante.cs
+++ estudiante.cs
@@ -1,20 +1,35 @@
+
 using System;
 
 public class Estudiante
 {
-   
     public string Nombre { get; set; }
     public int Edad { get; set; }
     public string Curso { get; set; }
 
-    
-   
+    public Estudiante(string nombre, int edad, string curso)
+    {
+        Nombre = nombre;
+        Edad = edad;
+        Curso = curso;
+    }
+
+    public void MostrarInformacion()
+    {
+        Console.WriteLine($"Nombre: {Nombre}, Edad: {Edad}, Curso: {Curso}");
+    }
 }
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("¡Hola, mundo!");
+        Estudiante estudiante1 = new Estudiante("Juan", 20, "Matemáticas");
+        Estudiante estudiante2 = new Estudiante("Ana", 22, "Física");
+
+        estudiante1.MostrarInformacion();
+        estudiante2.MostrarInformacion();
+
+        Console.WriteLine(datos tomados");
     }
 }
